060ae66a56a5388fe49424012c9061508a6bc0db
Jitae Kim <jitaekim93@gmail.com>
2018-10-04 10:32:57 -0700

    encapsulate analytics logging

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import styles from './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import ReactGA from 'react-ga';
-ReactGA.initialize('UA-73091430-2');
 
 ReactDOM.render(
     <App/>,
