c7a41a99c37ad2f2d1969a6ead84514ce9075cf7
Jitae Kim <jitaekim93@gmail.com>
2018-10-04 10:03:41 -0700

    Add google analytics

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import styles from './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-73091430-2');
 
 ReactDOM.render(
     <App/>,
